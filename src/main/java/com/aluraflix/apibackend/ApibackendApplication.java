97c21b84108bee11d3b07b2861e2856f50c56213
felipecabesssa <felipesoares100@hotmail.com>
2022-04-08 21:10:14 -0300

    API Backend - Projeto criado

---
+++ src/main/java/com/aluraflix/apibackend/ApibackendApplication.java
@@ -0,0 +1,13 @@
+package com.aluraflix.apibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluraflix/apibackend/ApibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluraflix.apibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
